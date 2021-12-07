89a4ed7eae74c3f536bed43ffb79adcf1fbac087
Rafael Teyral <r.teyral@tricentis.com>
2021-12-07 09:21:19 +0100

    cleanup

--- AdventOfCode21/Days/Seven.fs
+++ AdventOfCode21/Days/Seven.fs
@@ -2,36 +2,25 @@
 
 open CommonTypes
 
-type FuelConsumption = { Index: int; Position: int; Consumption: int }
-
-let initConsumption p i = { Index = i; Position = p; Consumption = 0 }
-
-let calcFuelConsumption (i : int) (positions : int []) =
-    let v = positions.[i]
-    positions |> Array.fold (fun acc el -> { acc with Consumption = acc.Consumption + abs(el - v) }) (initConsumption v i)
-
-let solve1 (puzzle: DailyPuzzle) =
-    let positions = puzzle.Lines.[0].Split(",") |> Array.map int
-    let fuelConsumptions = positions |> Array.mapi (fun x _ -> calcFuelConsumption x positions)
-    fuelConsumptions
-    |> Array.map (fun x -> x.Consumption)
-    |> Array.sort
-    |> Array.head
-
 let binomial v = (v * v + v) / 2
 
-let calcCrabsMovement position (crabs: int[]) =
-    crabs |> Array.fold (fun acc el -> acc + binomial(abs(el - position))) 0
+let calcCrabsMovement position (crabs: int[]) (f: int -> int) =
+    crabs |> Array.fold (fun acc el -> acc + f(abs(el - position))) 0
 
-let calcFuelConsumption2 (crabs : int []) (possiblePositions : int [])=
-    possiblePositions |> Array.map (fun x -> calcCrabsMovement x crabs)
+let calcFuelConsumption (crabs : int []) (possiblePositions : int []) (f: int -> int) =
+    possiblePositions |> Array.map (fun x -> calcCrabsMovement x crabs f)
 
-let solve2 (puzzle: DailyPuzzle) =
-    let positions = puzzle.Lines.[0].Split(",") |> Array.map int
-    let possiblePositions = [| Array.min positions .. Array.max positions |]
-    calcFuelConsumption2 positions possiblePositions
+let solve1 (puzzle: DailyPuzzle) =
+    let crabs = puzzle.Lines.[0].Split(",") |> Array.map int
+    calcFuelConsumption crabs crabs (fun x -> x)
     |> Array.sort
     |> Array.head
 
+let solve2 (puzzle: DailyPuzzle) =
+    let crabs = puzzle.Lines.[0].Split(",") |> Array.map int
+    let possiblePositions = [| Array.min crabs .. Array.max crabs |]
+    calcFuelConsumption crabs possiblePositions binomial
+    |> Array.sort
+    |> Array.head
 
 let solve puzzle = puzzle |> if puzzle.Part = 1 then solve1 else solve2
